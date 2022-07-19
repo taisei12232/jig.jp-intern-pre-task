7c0be796b7d4ffef712a9a76f0d0a76c1abb8581
taisei12232 <taisei12232@gmail.com>
2022-07-20 06:56:04 +0900

    入力チェックを強化

--- server.js
+++ server.js
@@ -41,9 +41,11 @@ serve(async (req) => {
       const docRef = doc(db, "shiritori", reqJson.id);
       const response = await getDoc(docRef)
       const data = response.data()
-      data.words.push(reqJson.word)
-      console.log(data)
-      await updateDoc(docRef,{words:data.words})
+      if(data.words.slice(-1)[0] === reqJson.word[0]){
+        data.words.push(reqJson.word)
+        console.log(data)
+        await updateDoc(docRef,{words:data.words})
+      }
       return new Response("200", {
           headers: {
               "content-type": "application/json",
