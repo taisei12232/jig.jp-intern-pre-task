1f97dd4ab2c0167671f445275e102ed460405f37
taisei12232 <taisei12232@gmail.com>
2022-07-20 13:56:14 +0900

    fix: typo

--- js/component/Shiritori.jsx
+++ js/component/Shiritori.jsx
@@ -61,7 +61,7 @@ const Shiritori = () => {
     }
     const handleReset = async () => {
         await fetch("/delete/world", {
-            mode: 'nno-cors',
+            mode: 'no-cors',
             method: "DELETE"
         });
     }
