f5b14cec1d04a8b0f132f0260b511973f86ec906
taisei12232 <taisei12232@gmail.com>
2022-07-20 15:47:14 +0900

    build

--- dist/index.6af55a572abc410f661e14fae357af3c.js
+++ dist/index.6af55a572abc410f661e14fae357af3c.js
@@ -8078,6 +8078,7 @@ Error generating stack: ` + i6.message + `
         const kogakiIndex = kogaki.findIndex((element) => element === data["words"].slice(-1)[0].slice(-1)[0]);
         console.log(kogakiIndex);
         console.log(big[kogakiIndex]);
+        console.log(word[0]);
         if (kogakiIndex != -1) {
           if (big[kogakiIndex] !== word[0] && data["words"].slice(-1)[0].slice(-1)[0] !== word[0])
             setResSentence("\u3057\u308A\u3068\u308A\u304C\u6210\u7ACB\u3057\u3066\u3044\u307E\u305B\u3093");
